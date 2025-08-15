2bee876cd376415686090ca10fb28a94f06c92d8
yubin012 <101037608+yubin012@users.noreply.github.com>
2025-08-16 00:44:48 +0900

    docker compose 설정

--- src/main/java/com/example/mindtrack/Config/SecurityConfig.java
+++ src/main/java/com/example/mindtrack/Config/SecurityConfig.java
@@ -1,28 +1,33 @@
 package com.example.mindtrack.Config;
 
+import com.example.mindtrack.Config.JWT.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+        private final JwtAuthFilter jwtAuthFilter;
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
+                                .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(
                                                                 "/swagger-ui/**",
                                                                 "/swagger-ui.html",
                                                                 "/v3/api-docs/**",
-                                                                "/upload-screenshot",
                                                                 "/api/auth/**")
                                                 .permitAll()
-                                                .anyRequest().authenticated());
-                http.csrf(csrf -> csrf.disable());
+                                                .anyRequest().authenticated())
+                                // JWT Filter를 UsernamePasswordAuthenticationFilter 전에 넣기
+                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
                 return http.build();
         }
