35648cbccb2350e7f6a61d7d44aafcff18d54192
yubin012 <101037608+yubin012@users.noreply.github.com>
2025-08-16 00:44:48 +0900

    docker compose 설정

--- src/main/java/com/example/mindtrack/Service/AdaptiveSamplingService.java
+++ src/main/java/com/example/mindtrack/Service/AdaptiveSamplingService.java
@@ -36,6 +36,7 @@ public class AdaptiveSamplingService {
 
     /**
      * 받은 이미지의 샘플링을 진행한다.
+     * 
      * @param userId
      * @param image
      * @return
@@ -45,47 +46,54 @@ public class AdaptiveSamplingService {
         Map<String, Object> response = new HashMap<>();
 
         // 유저아이디를 통해 유저를 찾는다
-        Users user = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Users user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
         // 이미지의 해시를 계산한다.
         long newHash = similarityCheckService.computeHash(image);
         log.info(">> 최근 들어온 이미지 hash 값 = {}", newHash);
 
-        try{
+        try {
             // 1차 샘플링 구간 제거
             // 2차 샘플링 구간 부터
             //
             // 직전 저장 이미지와의 유사도가 낮을 때
 
             // 지금 들어온 이미지의 dHash 해시값과 레디스 캐시 값의 해밍거리를 계산하여 유사한 해시값을 가져옴
-            Optional<ScreenshotImageCacheService.Candidate> mostSimilarFromCache = screenshotImageCacheService.findMostSimilarFromCache(user.getId(), newHash, 6);
+            Optional<ScreenshotImageCacheService.Candidate> mostSimilarFromCache = screenshotImageCacheService
+                    .findMostSimilarFromCache(user.getId(), newHash, 6);
 
-            if(mostSimilarFromCache.isPresent()){
+            if (mostSimilarFromCache.isPresent()) {
                 // 있다면, 그 해시값으로 해당 이미지 ScreenshotImage 가져오기
-                Optional<ScreenshotImage> ScreenshotImageOpt = screenshotImageRepository.findTopByUser_IdAndId(user.getId(), mostSimilarFromCache.get().imageId());
+                Optional<ScreenshotImage> ScreenshotImageOpt = screenshotImageRepository
+                        .findTopByUser_IdAndId(user.getId(), mostSimilarFromCache.get().imageId());
                 // 그리고 레디스 캐시에서 해당 해시 키를 가진 값(축소판 이미지)을 가져옴
-                Optional<byte[]> thumbBytesOpt = screenshotImageCacheService.getImageThumbByHash(user.getId(), mostSimilarFromCache.get().hash());
+                Optional<byte[]> thumbBytesOpt = screenshotImageCacheService.getImageThumbByHash(user.getId(),
+                        mostSimilarFromCache.get().hash());
 
-                if(ScreenshotImageOpt.isPresent() && thumbBytesOpt.isPresent()){
+                if (ScreenshotImageOpt.isPresent() && thumbBytesOpt.isPresent()) {
                     // 3차 샘플링 구간
                     //
-                    // 하지만, 해시 값이 같지만(dHash 계산으론 유사한 이미지이지만), 실제로 유사하지 않은 구조일 확률도 있으므로(타이핑이 더 됐다는 등),
+                    // 하지만, 해시 값이 같지만(dHash 계산으론 유사한 이미지이지만), 실제로 유사하지 않은 구조일 확률도 있으므로(타이핑이 더 됐다는
+                    // 등),
                     // 한 번 더 같은 해시값으로 가져와진 이미지와 지금 들어온 이미지와 SSIM 유사도 계산을 진행
 
                     // String prevSameHashS3url = optional.get().getS3url();
-                    // String prevSameHashFilePath = prevSameHashS3url.replace("http://localhost:8080/", "");
-                    // BufferedImage prevSameHashImage = ImageIO.read(new File(prevSameHashFilePath));
+                    // String prevSameHashFilePath =
+                    // prevSameHashS3url.replace("http://localhost:8080/", "");
+                    // BufferedImage prevSameHashImage = ImageIO.read(new
+                    // File(prevSameHashFilePath));
 
                     // 레디스 캐시에서 가져온 byte를 다시 이미지로 변환
                     byte[] thumbBytes = thumbBytesOpt.get();
-                    try(ByteArrayInputStream bais = new ByteArrayInputStream(thumbBytes)){
+                    try (ByteArrayInputStream bais = new ByteArrayInputStream(thumbBytes)) {
                         BufferedImage prevThumb = ImageIO.read(bais);
                         double reSimilarity = similarityCheckService.computeSimilarity(image, prevThumb);
                         log.info("[최종 선택된 유사 이미지와의 SSIM 유사도] : {}", reSimilarity);
 
                         // 그 유사도가 높다면, 거의 일치하는 이미지라고 판단
                         // 방문도를 높이고, 최근 방문 시간도 업데이트한다.
-                        if(reSimilarity >= SIMILARITY_THRESHOLD){
+                        if (reSimilarity >= SIMILARITY_THRESHOLD) {
                             ScreenshotImage prevScreenshotImage = ScreenshotImageOpt.get();
 
                             // 최근 방문 시간 및 방문도를 높인다.
@@ -96,29 +104,28 @@ public class AdaptiveSamplingService {
                             screenshotImageCacheService.cacheRecentImageHash(
                                     user.getId(),
                                     prevScreenshotImage.getId(),
-                                    prevScreenshotImage.getImageHash()
-                            );
+                                    prevScreenshotImage.getImageHash());
 
                             // AI 재분석 요청
                             return ResponseEntity.ok(Map.of(
                                     "success", true,
                                     "similarity", reSimilarity,
                                     "prevImageId", ScreenshotImageOpt.get().getId(),
-                                    "message", "저장된 적 있음! AI 재분석 요청: visitCnt=" + prevScreenshotImage.getVisitCnt()
-                            ));
-                        }else{
+                                    "message", "저장된 적 있음! AI 재분석 요청: visitCnt=" + prevScreenshotImage.getVisitCnt()));
+                        } else {
                             response.put("prevImageId", ScreenshotImageOpt.get().getId());
-                            response.put("message", "이전에 저장된 비슷한 이미지가 있지만(새 이미지 newHash와 유사도 9.7 이상 혹은 해밍거리 6이하가 있음), 실제로 구조가 다름(유사도SSIM가 낮음)");
+                            response.put("message",
+                                    "이전에 저장된 비슷한 이미지가 있지만(새 이미지 newHash와 유사도 9.7 이상 혹은 해밍거리 6이하가 있음), 실제로 구조가 다름(유사도SSIM가 낮음)");
                         }
                     }
 
-                }else{
+                } else {
                     response.put("message", "이전에 저장된 역사가 없음, 새 이미지 newHash와 같은 값이 아예 없음");
                 }
             }
             return saveScreenshotImage(image, user, newHash, response);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new CustomException("에러 발생: " + e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
@@ -126,17 +133,20 @@ public class AdaptiveSamplingService {
 
     // AWS S3 SDK 사용, 현재는 로컬 저장으로 구현되어있음
     // AWS 연동 이후 AmazonS3Clinet.putObject()식으로 변경
-/*    private String s3UploadAndReturnURL(BufferedImage image, String user_id) throws IOException {
-        String fileName = user_id + "_" + UUID.randomUUID() + ".png";
-        File file = new File("uploads/" + fileName);
-        file.getParentFile().mkdirs(); // 폴더 없으면 생성
-        ImageIO.write(image, "png", file);
+    /*
+     * private String s3UploadAndReturnURL(BufferedImage image, String user_id)
+     * throws IOException {
+     * String fileName = user_id + "_" + UUID.randomUUID() + ".png";
+     * File file = new File("uploads/" + fileName);
+     * file.getParentFile().mkdirs(); // 폴더 없으면 생성
+     * ImageIO.write(image, "png", file);
+     * 
+     * return "http://localhost:8080/uploads/" + fileName;
+     * }
+     */
 
-        return "http://localhost:8080/uploads/" + fileName;
-    }*/
-
-    private byte[] imageToBytes(BufferedImage image){
-        try(ByteArrayOutputStream baos = new ByteArrayOutputStream()){
+    private byte[] imageToBytes(BufferedImage image) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             ImageIO.write(image, "png", baos);
             return baos.toByteArray();
         } catch (IOException e) {
@@ -144,8 +154,9 @@ public class AdaptiveSamplingService {
         }
     }
 
-    private ResponseEntity<Map<String, Object>> saveScreenshotImage(BufferedImage image, Users user, Long newHash, Map<String, Object> response) throws IOException {
-        //String s3Url = s3UploadAndReturnURL(image, user.getUserId());
+    private ResponseEntity<Map<String, Object>> saveScreenshotImage(BufferedImage image, Users user, Long newHash,
+            Map<String, Object> response) throws IOException {
+        // String s3Url = s3UploadAndReturnURL(image, user.getUserId());
         byte[] byteData = imageToBytes(image);
 
         ScreenshotImage newScreenshot = ScreenshotImage.builder()
@@ -164,8 +175,7 @@ public class AdaptiveSamplingService {
                 user.getId(),
                 newScreenshot.getId(),
                 newScreenshot.getImageHash(),
-                byteData
-        );
+                byteData);
 
         response.put("currentImageId", newScreenshot.getId());
 
