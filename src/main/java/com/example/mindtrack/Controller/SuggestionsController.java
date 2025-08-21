cf856174a87b2d09704426cf5902d94c62690b51
정하은 <he3080@naver.com>
2025-08-21 17:39:49 +0900

    [refact] suggestion user_id 잘못된 값으로 찾던 오류 수정 - suggestion테이블에 저장되는 user_id는 users테이블의 PK 값이지만, users의 PK id는 Long타입이고, suggestion의 user_id는 TEXT 타입이다. - 따라서 string user_id로 user를 먼저 찾아, pk 아이디를 찾고, 그걸 string 변환 후 suggestion을 찾게 수정

--- src/main/java/com/example/mindtrack/Controller/SuggestionsController.java
+++ src/main/java/com/example/mindtrack/Controller/SuggestionsController.java
@@ -2,7 +2,9 @@ package com.example.mindtrack.Controller;
 
 import com.example.mindtrack.Config.JWT.JwtUtil;
 import com.example.mindtrack.DTO.SuggestionPayload;
+import com.example.mindtrack.Domain.Users;
 import com.example.mindtrack.Repository.SuggestionRepository;
+import com.example.mindtrack.Repository.UserRepository;
 import com.example.mindtrack.SSE.SuggestionSseHub;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +23,20 @@ import java.io.IOException;
 public class SuggestionsController {
     private final SuggestionSseHub hub;
     private final SuggestionRepository suggestionRepository;
+    private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
     @GetMapping("/latest")
     public ResponseEntity<SuggestionPayload> latest(Authentication authentication) {
         String userId = (String) authentication.getPrincipal(); // principal은 userId 문자열
-        return ResponseEntity.of(suggestionRepository.findLatestSuggestionPayloadByUser(userId));
+        log.info("suggestion latest 로그: Authentication 유저아이디 ={}", userId);
+
+        Users user = userRepository.findByUserId(userId).orElseThrow();
+        String userIdStr = String.valueOf(user.getId());
+
+        log.info("suggestion latest 로그: Authentication 유저 bigint -> text 아이디 ={}", userIdStr);
+
+        return ResponseEntity.of(suggestionRepository.findLatestSuggestionPayloadByUser(userIdStr));
     }
 
     @GetMapping(value="/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -35,7 +45,13 @@ public class SuggestionsController {
             @RequestHeader(value="Last-Event-ID", required = false) String lastEventId
     ) throws IOException {
         log.info("token: {}", token);
-        String userId = jwtUtil.extractUserId(token); // ✅ JWT에서 userId 파싱
-        return hub.subscribe(userId);
+        String userId = jwtUtil.extractUserId(token);
+
+        Users user = userRepository.findByUserId(userId).orElseThrow();
+        String userIdStr = String.valueOf(user.getId());
+
+        log.info("suggestion stream 로그: 토큰에서 추출한 아이디(숫자)={}", userIdStr);
+
+        return hub.subscribe(userIdStr);
     }
 }
